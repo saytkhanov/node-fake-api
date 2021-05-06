8b8696cb7fcae06c468afa03d926b6ac9df90d92
saytkhanov <saythanov2014@mail.ru>
2021-05-06 11:44:02 +0300

    изменения

--- functions.js
+++ functions.js
@@ -1,54 +1,37 @@
-// const http = require ('http')
-// const fs = require('fs');
-// const path = require('path');
-//
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200)
-//   fs.readFile(path.resolve(__dirname, 'database.json'), 'utf-8', (err, data) => {
-//     let change = JSON.parse(data);
-//
-//     if (err) {
-//       console.log('manty')
-//     } else {
-//       if(req.method === 'GET') {
-//         const parse = parseQuery(req.url);
-//
-//         if(parse.id === null ) {
-//           res.write(JSON.stringify(change[parse.resource]))
-//           res.end()
-//         }
-//     }
-//   }
-// })
-// server.listen(3000)
-//
-// let getIndexById = (collection, id) => {
-//  if(collection[id - 1] === undefined) {
-//    return - 1
-//  } else {
-//    return collection[id - 1]
-//  }
-// }
-//
-//
-// let getNextId = collection => {
-//  let max = 0;
-//  for(let i = 0 ; i < collection.length; i ++) {
-//    max = collection[i].id
-//  } if(collection.length < 0) {
-//    return 1
-//   }
-//  return max + 1
-// }
-//
-// parseQuery = string => {
-//  let split = string.split('/');
-//  let slovo = split[1];
-//  let chislo = split[2];
-//
-//  if(chislo <= 0 || chislo === undefined) {
-//    return { resource:slovo, id:null }
-//  } else {
-//    return { resource:slovo, id:Number(chislo) }
-//  }
-// }
+const getIndexById = (collection, id) => {//создаем функцию, которая присваивает 2 параметра
+  for(let i = 0; i < collection.length; i++){//запускаем цикл
+    if(collection[i].id === id) {//если ключ id объекта collection
+      return i// возвращаем ключ
+    }
+  }
+  return -1// иначе -1
+}
+
+const getNextId = (collection) => {// cоздаем фукнцию, которая будет увеличивать индекс, смотря на самое большое значение
+  let value = 0// создаем переменную с нулевым значение
+  for (let i = 0; i < collection.length; i++) {//запускаем цикл
+    if (collection[i].id > value)//если id больше переменной
+      value = collection[i].id//в нашу переменную прибавляем id
+  }
+  if (collection.length < 0) {//если длина меньше 0
+    return 1// то возравращаем 1
+  }
+  return value + 1// в общем возвращаем bigValue и прибавляем 1
+}
+
+
+const parseQuery = string => {//пишем функцию,которая будет определять что возвращается при определенных запросах
+  let splits = string.split('/');// созадем переменную где разбиваем на массив, разделяя их cлэшом
+  let word = splits[1];// создаем переменную, которая берет 1 значение
+  let num = splits[2];// создаем переменную, которая берет 2 значение
+  if(num <= 0 || num === undefined) {//если num меньше или равно нулю или равно undefined
+    return { resourse:word, id:null}// возвращаем значение с id null
+  } else {
+    // иначе num переводим в число и добавляем
+    return { resourse:word, id:Number(num)}
+  }
+}
+
+module.exports.getIndexById = getIndexById;
+module.exports.getNextId = getNextId;
+module.exports.parseQuery = parseQuery;
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const { getIndexById, getNextId, parseQuery} = require('./functions')
+
+const fileName = path.resolve(__dirname, 'database.json')
 
 const server = http.createServer((req, res) => {//создаем сервер
   res.writeHead(200);//код сайта, 200 означает что все хорошо
 
-  fs.readFile(path.resolve(__dirname, 'database.json'), 'utf-8', (err, data) => {//читаем файл
+  fs.readFile(fileName, 'utf-8', (err, data) => {//читаем файл
     data = JSON.parse(data)//данные из файла парсим в объект
     if(err) {//если ошибка
       console.log(err)//вызываем в консоль ошибку
@@ -27,7 +30,7 @@ const server = http.createServer((req, res) => {//создаем сервер
         const parse = parseQuery(req.url);//опять добавляем переменную
         const index = getIndexById(data[parse.resourse], parse.id)
         data[parse.resourse].splice(index, 1);//берем данные и используем метод splice для удаления
-        fs.writeFile(path.resolve(__dirname, 'database.json'), JSON.stringify(data), (err, data) => {//перезаписываем файл
+        fs.writeFile(fileName, JSON.stringify(data), (err, data) => {//перезаписываем файл
           res.end('Удаление прошло успешно')// в случае успеха выводим текст
         })
       }
@@ -44,8 +47,15 @@ const server = http.createServer((req, res) => {//создаем сервер
           chunk.toString();//данные преобразовываем в строку
           chunk.id = i;//присвываем параметр, чтобы добавлял новый индекс
           data[parse.resourse].push(chunk);//в базу данных пушим в конец преобразованные данные
-          fs.writeFile(path.resolve(__dirname, 'database.json'), JSON.stringify(data, null, ' '), () => {
-            res.end('Добавлено');// перезаписываем в строку данные и в случае успеха выводим Добавлено
+          fs.writeFile(fileName, JSON.stringify(data, null, ' '), (err) => {
+            if(err) {
+              res.writeHead(404);
+              res.write('ошибка запроса')
+            } else {
+              res.writeHead(200);
+              res.write('Добавлено')
+            }
+            res.end();// перезаписываем в строку данные и в случае успеха выводим Добавлено
           })
         })
       }// если метод PATCH
@@ -53,9 +63,18 @@ const server = http.createServer((req, res) => {//создаем сервер
         const parse = parseQuery(req.url);// опять же используем переменные
         const index = getIndexById(data[parse.resourse], parse.id);
         req.on('data', update => {//запускаем обработчик событий, которое читает содержимое файлы в качестве параметра используем update
-        update = JSON.parse(update);// данные парсим
-        data[parse.resourse][index] = Object.assign(data[parse.resourse][index], update)//объединение старого и нового значения, для этого используем Object.assign по подсказке Aхмеда
-        fs.writeFile(path.resolve(__dirname, 'database.json'), JSON.stringify(data, null, ' '), () => {})// перезаписываем файл переводя в строку, так добавляем значение, чтобы все это дело вырлвнял в файле
+          update = JSON.parse(update);// данные парсим
+          data[parse.resourse][index] = Object.assign(data[parse.resourse][index], update)//объединение старого и нового значения, для этого используем Object.assign по подсказке Aхмеда
+          fs.writeFile(fileName, JSON.stringify(data, null, ' '), (err) => {
+            if(err) {
+              res.writeHead(404);
+              res.write('Ошибка запроса')
+            } else {
+              res.writeHead(200);
+              res.write('Успешно')
+            }
+            res.end()
+          })// перезаписываем файл переводя в строку, так добавляем значение, чтобы все это дело вырлвнял в файле
         })
       }
     }
@@ -63,36 +82,3 @@ const server = http.createServer((req, res) => {//создаем сервер
 })
 server.listen(3000)
 
-const getIndexById = (collection, id) => {//создаем функцию, которая присваивает 2 параметра
-for(let i = 0; i < collection.length; i++){//запускаем цикл
-  if(collection[i].id === id) {//если ключ id объекта collection
-    return i// возвращаем ключ
-  }
-}
-return -1// иначе -1
-}
-
-const getNextId = (collection) => {// cоздаем фукнцию, которая будет увеличивать индекс, смотря на самое большое значение
-  let bigValue = 0// создаем переменную с нулевым значение
-  for (let i = 0; i < collection.length; i++) {//запускаем цикл
-    if (collection[i].id > bigValue)//если id больше переменной
-      bigValue = collection[i].id//в нашу переменную прибавляем id
-  }
-  if (collection.length < 0) {//если длина меньше 0
-    return 1// то возравращаем 1
-  }
-  return bigValue + 1// в общем возвращаем bigValue и прибавляем 1
-}
-
-
-const parseQuery = string => {//пишем функцию,которая будет определять что возвращается при определенных запросах
-  let splits = string.split('/');// созадем переменную где разбиваем на массив, разделяя их cлэшом
-  let word = splits[1];// создаем переменную, которая берет 1 значение
-  let num = splits[2];// создаем переменную, которая берет 2 значение
-  if(num <= 0 || num === undefined) {//если num меньше или равно нулю или равно undefined
-    return { resourse:word, id:null}// возвращаем значение с id null
-  } else {
-    // иначе num переводим в число и добавляем
-    return { resourse:word, id:Number(num)}
-  }
-}
